ec8ca3faffc3005f6a3295c8daa06883aeb74650
Leonardo Balter <leonardo.balter@gmail.com>
2013-08-24 16:43:56 -0300

    comecando a processar json

--- js/ddgapi.js
+++ js/ddgapi.js
@@ -1,4 +1,25 @@
 jQuery(function($) {
+    var resultsArea = $( '.search-results' );
+
+    function processTopics( topics ) {
+        var listItem;
+
+
+        topics.forEach(function( topic ) {
+            $( '<li></li>' )
+                .html( topic.Result )
+                .appendTo( resultsArea );
+
+        });
+
+    }
+
+    function processDefinition( definition, source, sourceURL ) {
+        $( '<h2></h2>' ).html( definition ).appendTo( resultsArea );
+        $( '<h3></h3>' ).html( 'Source: <a href="' + sourceURL + '">' + source + '</a>' ).appendTo( resultsArea );
+        $( '<hr/>' ).appendTo( resultsArea );
+    }
+
     $( '#ddg-form' ).on( 'submit', function(e) {
         e.preventDefault();
         var self = $( this );
@@ -9,7 +30,19 @@ jQuery(function($) {
             dataType     : 'jsonp',
             jsonCallback : 'ffos',
             success      : function( data ) {
-                console.log( data );
+                $( '.search-results' ).empty();
+
+                if ( data.Definition ) {
+                    processDefinition( data.Definition, data.AbstractSource, data.AbstractURL );
+                }
+
+                if ( data.Topics ) {
+                    processTopics( data.Topics );
+                }
+
+                if ( data.RelatedTopics ) {
+                    processTopics( data.RelatedTopics );
+                }
             },
             error        : function( data ) {
                 console.log( 'meep... error... meep...' );
