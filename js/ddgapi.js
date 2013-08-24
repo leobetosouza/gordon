6c81d30eb53051ee3d5f4d43a828ce1eea1845ab
Leonardo Balter <leonardo.balter@gmail.com>
2013-08-24 17:22:39 -0300

    aaaa

--- js/ddgapi.js
+++ js/ddgapi.js
@@ -1,23 +1,40 @@
 jQuery(function($) {
     var resultsArea = $( '.search-results' );
 
-    function processTopics( topics ) {
+    function processTopic( topic ) {
         var resultList = $( '<ul></ul>'),
-            listItem;
+            listItem,
+            resultText = '';
 
-        topics.forEach(function( topic ) {
-            $( '<li></li>' )
-                .html( topic.Result )
-                .appendTo( resultList );
-        });
+        if ( topic.Icon && topic.Icon.URL ) {
+            resultText = '<aside class="pack-end"><img alt="placeholder" src="' + topic.Icon.URL + '"></aside>';
+        }
+
+        resultText += '<a href="' + topic.FirstURL + '"><p>' + topic.Text + '</p></a>';
+
+        $( '<li></li>' )
+            .html( resultText )
+            .appendTo( resultList );
 
         resultList.appendTo( resultsArea ); 
+    }
+
+    function processResults( topics ) {
+        topics.forEach(function( topic ) {
+            var resultText = '';
+
+            if ( topic.Topics ) {
+                processResults( topic.Topics );
+            } else if ( topic.Result ) {
+                processTopic( topic );
+            }
+        });
 
     }
 
     function processDefinition( definition, source, sourceURL, heading ) {
         if ( heading ) {
-            $( '<h1></h1>' ).html( heading ).appendTo( resultsArea );
+            $( '<header></header>' ).html( heading ).appendTo( resultsArea );
         }
         $( '<h2></h2>' ).html( definition ).appendTo( resultsArea );
         $( '<h3></h3>' ).html( 'Source: <a href="' + sourceURL + '">' + source + '</a>' ).appendTo( resultsArea );
@@ -34,18 +51,23 @@ jQuery(function($) {
             dataType     : 'jsonp',
             jsonCallback : 'ffos',
             success      : function( data ) {
+                if ( data.Redirect ) {
+                    window.location.href = data.Redirect;
+                }
+
                 $( '.search-results' ).empty();
 
                 if ( data.Definition ) {
                     processDefinition( data.Definition, data.AbstractSource, data.AbstractURL, data.Heading );
                 }
 
-                if ( data.Topics ) {
-                    processTopics( data.Topics );
-                }
-
                 if ( data.RelatedTopics ) {
-                    processTopics( data.RelatedTopics );
+                    processResults( data.RelatedTopics );
+                    if ( resultsArea.text() ) {
+                        resultsArea.preppend( '<header>I also found...</header>' );
+                    } else {
+                        resultsArea.html( '<header>No clues so far...</header><div id="gordon_image"><img src="images/gordon.jpg"></div>' );
+                    }
                 }
             },
             error        : function( data ) {
